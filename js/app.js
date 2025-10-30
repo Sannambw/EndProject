a35abb611a5680862a5a3207cc6f56ddbed1c0b5
Sannambw <sannambw@gmail.com>
2025-10-30 16:58:22 +0100

    alla komentarer klara

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 /* En lista med de filmgenrer som används i menyn */
 const genrer = ["drama", "comedy", "animation"];
 
-/* Här sparas alla filmer som hämtas från API:et */
+/* Här sparas alla filmer som hämtas från API:et jag valde */
 let allaFilmer = [];
 
-/* Funktionen hämtar filmer från API:et.
+/* Funktionen hämtar filmer från API:et jag valde.
 Datan laddas från webbsidan och visar antingen alla genrer
-eller bara den specifika genre man väljer */
+eller bara den specifika genre som man väljer */
 async function hamtaFilmer(genre) {
   const resultat = document.getElementById("resultat");
-  resultat.innerHTML = "<p>Laddar filmer...</p>"; /* Tillfällig text medan filmer hämtas */
+  resultat.innerHTML = "<p>Laddar filmer...</p>"; /* Tillfällig text medan filmer hämtas och kommer upp på sidan */
   let filmer = [];
 
-  /* Om man väljer “Alla filmer” hämtas alla tre genrer (drama, comedy och animation) */
+  /* Om man väljer “Alla filmer” hämtas alla tre genrer (drama, komedi och animation) */
   if (genre === "all") {
     for (let g of genrer) {
       /* Hämtar filmer för varje genre i listan */
@@ -35,13 +35,13 @@ async function hamtaFilmer(genre) {
 
 /* Funktion för att visa filmer på webbsidan
    - Visar filmens titel och bild
-   - Sätter rubrik beroende på vald genre
+   - Väljer rätt rubrik beroende på vald genre
    - Visar filmerna i rader med 4 kort per rad */
 function visaFilmer(genre) {
   const resultat = document.getElementById("resultat");
   resultat.innerHTML = ""; /* Tömmer innehållet innan nya filmer visas */
 
-  /* Bestämmer vilken rubrik som ska stå på sidan */
+  /* Bestämmer vilken rubrik som ska stå på sidan och får de till det rätta språket */
   let rubrik = "";
   if (genre === "all") rubrik = "Alla filmer";
   else if (genre === "drama") rubrik = "Drama";
@@ -53,10 +53,11 @@ function visaFilmer(genre) {
   titel.textContent = rubrik;
   resultat.appendChild(titel);
 
-  /* Skapar rader med 4 filmer i varje rad */
+  /* Skapar rader med 4 filmer i varje rad så att det ser
+  symmetriskt och användarvänligt ut */
   let rad;
   allaFilmer.forEach((film, index) => {
-    /* Startar en ny rad var fjärde film */
+    /* Startar en ny rad var fjärde film, så att det är användarvänlig för besökaren.*/
     if (index % 4 === 0) {
       rad = document.createElement("div");
       rad.classList.add("rad"); /* Klass för raderna */
